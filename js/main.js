208784bf81255dc086cd51211bfcdcb917c831b5
satya k <satyanarayna121@gmail.com>
2023-03-04 15:17:36 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+document.querySelector('button').addEventListener('click',changeQuote);
+
+const imageElem = document.querySelector('#image');
+const authorElem = document.querySelector('#author');
+
+function changeQuote(){
+  const index = Math.floor(Math.random()*photos.length);
+  console.log(imageElem ,photos[index])
+  imageElem.innerHTML = `<img class="img-fluid"  src="${photos[index].download_url}" alt="">`
+  authorElem.textContent = photos[index].author;
+  
+};
+
+let photos = [];
+
+fetch("https://picsum.photos/v2/list/")
+  .then(response => response.json()) // Parse the JSON data
+  .then(data => {
+    photos.push(...data)
+  })
+  .catch(error => {
+    console.error(error);
+  });
